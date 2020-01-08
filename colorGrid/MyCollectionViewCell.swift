f7a03a9dd8c416f6ebbdd2ed9b241e38e758c237
sandeep <sandeep.mhal6@gmail.com>
2020-01-08 18:50:55 -0500

    new

---
+++ colorGrid/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  colorGrid
+//
+//  Created by SanDEV on 2020-01-08.
+//  Copyright © 2020 SanDEV. All rights reserved.
+//
+
+import UIKit
+class MyCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var myLabel: UILabel!
+}
--- colorGrid/ViewController.swift
+++ colorGrid/ViewController.swift
@@ -5,16 +5,39 @@
 //  Created by SanDEV on 2020-01-08.
 //  Copyright © 2020 SanDEV. All rights reserved.
 //
-
 import UIKit
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
-class ViewController: UIViewController {
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
+     let cc = [UIColor(red: CGFloat(160/255), green: CGFloat(183.0/255), blue: CGFloat(227.0/255), alpha: 1), UIColor(red: CGFloat(160/255), green: CGFloat(183.0/255), blue: CGFloat(227.0/255), alpha: 1)]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    // MARK: - UICollectionViewDataSource protocol
+
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
     }
 
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.myLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.
+        // make cell more visible in our example project
+
+        return cell
+    }
+
+    // MARK: - UICollectionViewDelegate protocol
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
 }
-
